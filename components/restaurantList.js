086a3af86f6bc58f87866e0aa178e21fc96f7ee6
denakIST <denizakar2018@gmail.com>
2023-07-26 19:31:21 -0400

    logging picture URLs

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -75,6 +75,7 @@ function RestaurantList(props) {
 
   if (searchQuery.length > 0) {
     const restList = searchQuery.map((res) => (
+      console.log(`picURL ${res.attributes.image.data.attributes.url}`)
       <Col xs="6" sm="4" key={res.id}>
         <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
           <CardImg
